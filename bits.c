96193c6f3cf458da0cbe49b2b6cc94d247440f33
willforcecomputers <willforcecomputers@gmail.com>
2016-10-11 14:32:22 -0700

    Created files and finished funtion 1
    Initial setup and creation of function 1.

---
+++ bits.c
@@ -0,0 +1,32 @@
+/*
+ * bits.c
+ *
+ *  Created on: Oct 11, 2016
+ *      Author: William Metzger
+ *  Create various function to solve puzzles with a limited toolset.
+ */
+
+#include <stdio.h>
+
+int func1(int x, int y) {
+	//this function computes the bitwise & of two numbers only using ~ and |.
+	//To accomplish this, each number is applied a bitwise not(~) and then combined with a bitwise or(|), then another bitwise not is applied to the resulting number.
+	int num1 = x;
+	int num2 = y;
+	int bitand;
+
+	num1 = ~num1;
+	num2 = ~num2;
+	bitand = num1 | num2;
+	bitand = ~bitand;
+
+	return bitand;
+}
+
+int main() {
+	int x;
+	int y;
+	printf("The bitwise & of 5 and 6 is %d\n", func1(5, 6));
+
+	return 1;
+}
---
+++ function2.c
@@ -0,0 +1,17 @@
+/*
+ * function2.c
+ *
+ *  Created on: Oct 11, 2016
+ *      Author: William
+ */
+
+#include <stdio.h>
+
+
+
+int main() {
+	int x;
+	int y;
+
+	return 1;
+}
